652396c1a39b3e235dac0ff746f974aba28961d1
Natália Condê <natalia.csouza98@outlook.com>
2019-05-12 16:13:49 -0300

    Delete and update objects

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -10,6 +10,8 @@ import {
   TouchableOpacity
 } from 'react-native';
 
+import { Dialog } from 'react-native-simple-dialogs';
+
 import { Right } from "native-base";
 
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -17,7 +19,18 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import Parse from "parse/react-native";
 
 export default class HomeScreen extends React.Component {
-
+  constructor(props){
+    super(props);
+    this.state = {
+      results: [],
+      item: '',
+      nameErrorNew: null,
+      nameErrorUpdate: null,
+      dialogVisible: false,
+      updateValueDialog: '',
+      idObject: ''
+    }
+  }
   static navigationOptions = ({ navigation }) => {
     return {
       headerTitle: 'List',
@@ -51,14 +64,6 @@ export default class HomeScreen extends React.Component {
     };
   };
 
-
-  constructor(props){
-    super(props);
-    this.state = {
-      results: []
-    }
-  }
-
   componentWillMount(){
     Parse.User.currentAsync().then(user => {
       if (user == undefined) {
@@ -88,9 +93,25 @@ export default class HomeScreen extends React.Component {
     )
   }
 
-
   _onPress = (id) => {
-    alert(id)
+    Alert.alert(
+      "Choose an action",
+      '',
+      [
+        {text: 'Delete', onPress: () => {
+          this.setState({idObject: id});
+          this._onPressDeleteObject()}},
+        {text: 'Update', onPress: () => this.setState({dialogVisible: true})},
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        }       
+      ],
+      {cancelable: false}
+    );
+
+   
   }
 
   getAllData = async () => {
@@ -101,17 +122,59 @@ export default class HomeScreen extends React.Component {
     this.setState({results:resultQuery}); 
   }
 
+  addItem = () => {
+    let item = this.state.item;
+
+    if (item.trim() === "" ) {
+      this.setState(() => ({ nameError: `Fill the fields correctly.` }));
+    } else {
+      const Todo = Parse.Object.extend("Todo");
+      const todo = new Todo();
+
+      todo.set("task", this.state.item);
+      todo.set("userId",  Parse.User.current());
+      console.log(this.state.item)
+      
+      todo.save().then(object => {
+        this.getAllData();
+      }).catch(error=> {console.log(error)})
+    }
+  }
+
+  updateItem = () => {
+    const query = new Parse.Query("Todo");
+    query.get(this.state.idObject).then(object => {
+      object.set("task", this.state.item);
+      object.save().then(objUpdate => {
+        this.getAllData();
+        this.setState({dialogVisible: false})
+      });
+    });  
+  }
+
+
+  _onPressDeleteObject = async() => {
+    const query = new Parse.Query("Todo");
+    const object = await query.get(this.state.idObject);
+    try{
+      object.destroy();
+      this.getAllData();
+    } catch (e){
+      alert(e)
+    }
+  }
+
   renderItem = ({item, separators}) => {
     return (
         <TouchableOpacity
           style={styles.borderFlatListItens}
-          onPress={() => this._onPress(item.id)}>
+          onPress={() => {
+            this._onPress(item.id);
+            this.setState({updateValueDialog:item.get("task")})
+          }}>
           <View>
               <Text>{item.get("task")}</Text>
           </View>
-          <Right>
-            <Icon name="minus" size={16} color='#00b5ec' style={{}}/>
-          </Right>
         </TouchableOpacity>
     )
   }
@@ -123,9 +186,20 @@ export default class HomeScreen extends React.Component {
           <View style={styles.containerInputs}> 
             <TextInput style={styles.inputTask}
               placeholder="Add tasks...."
-              underlineColorAndroid='transparent' />
-            <Icon type="Feather" name='plus' style={styles.icon} onChangeText={(password) => this.setState({password})}/>
+              underlineColorAndroid='transparent'
+              ref={'inputObject'}
+              onChangeText={(text) => this.setState({item: text, nameError: null})}/>
+            <Icon 
+                type="Feather"
+                name='plus' 
+                style={styles.icon}                
+                onPress={() => this.addItem()}/>
           </View>
+          {!!this.state.nameErrorNew && (
+            <View styles={styles.divError}>
+                <Text style={styles.divErrorFont}>{this.state.nameErrorNew}</Text>
+            </View>
+          )}
           <View style={{padding: 30, flex: 1}}>
             <Text style={styles.titleItems}>Items</Text>
             <FlatList
@@ -136,6 +210,28 @@ export default class HomeScreen extends React.Component {
               keyExtractor={(item, index) => index.toString()}
             />
           </View>
+          <Dialog
+            visible={this.state.dialogVisible}
+            title="Update object"
+            onTouchOutside={() => this.setState({dialogVisible: false})} >
+            <View style={styles.containerInputs}> 
+              <TextInput style={styles.inputTask}
+                placeholder="Add tasks...."
+                underlineColorAndroid='transparent'
+                value={this.state.updateValueDialog}
+                onChangeText={(text) => this.setState({item: text, nameErrorUpdate: null})}/>
+              <Icon 
+                  type="Feather"
+                  name='plus' 
+                  style={styles.icon}                
+                  onPress={() => this.updateItem()}/>
+            </View>
+            {!!this.state.nameErrorUpdate && (
+              <View styles={styles.divError}>
+                  <Text style={styles.divErrorFont}>{this.state.nameErrorUpdate}</Text>
+              </View>
+            )}
+          </Dialog>
         </ScrollView>        
       </View>
     );
@@ -167,13 +263,23 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     margin: 20,
-    fontSize: 20,
+    fontSize: 40,
     color: '#00b5ec'
   },
   titleItems: {
     fontWeight: 'bold',
     fontSize: 25,
     marginBottom: 25
+  },
+  divErrorFont:{
+    textAlign: 'center',
+    color: '#721c24',
+    backgroundColor: '#f8d7da',
+    borderColor: '#f5c6cb',
+    padding: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    borderWidth: 2,
   },
   borderFlatListItens:{ 
     flexDirection: 'row', 
